f7b4b08a6715fb70cca079cc42b33df20c9df897
Jon Lake <jlake21@ford.com>
2022-03-22 13:34:00 -0600

    Add functions

--- exercise.js
+++ exercise.js
@@ -1,8 +1,28 @@
-// Your code goes here
+function getItemByID(items, id){
+    const itemsByID = items.find(item => item.id === id)
+    return itemsByID
+}  
+
+function buildTransactions(items, sales){
+    sales.map(sale => {
+        const itemId = sale.itemId
+        const item = getItemByID(items, itemId)
+
+        return {
+            ...sale, 
+            description: item.description,
+            price: item.price
+        }
+    })
+}
+
+function getTransactionsByItemDescription(transactions, itemDescription){
+    return transactions.filter(transaction => transaction.description === itemDescription);
+}
 
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+    getItemById,
+    buildTransactions,
+    getTransactionsByItemDescription
 }
